81aa31e8f867b871b6c245419a7e48918cb47f65
Jose Cassano <cassanojoseluis@gmail.com>
2020-01-22 17:09:41 -0300

    Start .net proyect of tarea

---
+++ tarea-desktop/Form2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace tarea_desktop
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e) => this.Close();
+    }
+}
